2ec440d34dcd94820dd4498d96d989666f78704b
Nandhagopan <nandhagopan102@gamil.com>
2024-07-02 14:43:43 +0530

    changes some files

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+
+function Header() {
+    return (
+        <>
+            <header>
+                <h1>hey</h1>
+                <h2>hemmeh</h2>
+                <p>hello</p>
+            </header>
+        </>
+    );
+}
+
+export default Header
